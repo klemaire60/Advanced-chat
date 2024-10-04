308dc393631dc1a9f0d5e4473b236d88df36209d
klemaire60 <kekelele6020@gmail.com>
2024-10-04 20:05:40 +0200

    Route /register + verification des champs + MAJ config.json

--- API/server.js
+++ API/server.js
@@ -9,10 +9,10 @@ const cookieParser = require('cookie-parser');
 const mysql = require('mysql');
 
 // Fichier de configuration des paramètres
-const config = require('./config');
+const config = require('./config.json');
 
 // Paramètres de la base de données
-connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: config.database.host,
     user: config.database.user,
     password: config.database.password,
@@ -44,6 +44,83 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(Limiter);
+
+// Fonction pour vérifier les inputs pour éviter les injections SQL ou XSS
+function containsInvalidChars(input) {
+    const forbiddenChars = /['";<>]/; // Liste des caractères interdits
+    return forbiddenChars.test(input); // Renvoie true si un des caractères est trouvé
+}
+
+app.post('/register', (req, res) => {
+    const { mail, username, password } = req.body;
+    if(!mail || !username || !password) return res.status(409).json({ message : "Un des champs est manquant" });
+    
+    let errorMessage ='';
+    
+    // Vérification de chaque champs
+    if(containsInvalidChars(mail)) {
+        errorMessage += "Le mail contient des caractères interdits. "
+    }
+
+    if(containsInvalidChars(username)) {
+        errorMessage += "Le nom d'utilisateurs contient des caractères interdits. "
+    }
+
+    if(containsInvalidChars(password)) {
+        errorMessage += "Le mot de passe contient des caractères interdits."
+    }
+
+    // Si un des champs contient un caractères interdits alors on renvoie la requête
+    if(errorMessage !== "") {
+        return res.status(401).json({ message : errorMessage });
+    }
+
+    const sqlVerif = 'SELECT idUser FROM user WHERE mail = ? OR username = ?'
+
+    connection.query(sqlVerif, [mail, username], (err, result) => {
+        if(err) { 
+            console.error('Erreur lors de la requête sqlVerif\nErreur : \n', err);
+            return res.status(500).json({ message : "Erreur lors de la création du compte" });
+        }
+
+        if(result.lenght > 0) {
+            return res.status(401).json({ message : "Le mail est déjà utilisé "});
+        }
+
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) {
+                console.error("Erreur lors de la génération du sel Bcrypt\nErreur Bcrpt Salt:\n", err);
+                return res.status(500).json({ message: "Erreur lors de la création du compte" });
+            }
+
+            bcrypt.hash(password, salt, (hashErr, hashedPassword) => {
+                if(hashErr) {
+                    console.error("Erreur lors du hachage du mot de passe\nErreur : \n", hashErr);
+                    return res.status(500).json({ message : "Erreur lors de la création du compte" });
+                }
+
+                let token = jwt.sign(mail, config.jwtKey);
+
+                const sqlNewUser = 'INSERT INTO user (username, mail, password, token) VALUES (?,?,?,?)';
+
+                connection.query(sqlNewUser, [username, mail, hashedPassword, token], (err) => {
+                    if(err) {
+                        console.error("Erreur lors de la création du nouvel User\nErreur sql :\n", err);
+                        return res.status(500).json({ message: "Erreur lors de la création du compte" });
+                    }
+
+                    res.cookie('userToken', token, {
+                        httpOnly : true,
+                        secure: false,
+                        maxAge: 43200000
+                    })
+                    
+                    return res.status(200).json({ message: "Compte créé avec succès" });
+                })
+            })
+        })
+    })
+})
 
 // Lancement du serveur
 app.listen(config.port, () => {
